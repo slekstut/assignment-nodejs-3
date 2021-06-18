17391d13566c1467fe710ef6372b895311d9ba18
Sarunas <sarunas.lekstutis@gmail.com>
2021-06-18 12:58:05 +0300

    first commit

---
+++ routes/users.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const express = require('express');
+const rootDir = require('../util/path');
+
+const router = express.Router();
+
+router.get('/users', (req, res, next) => {
+ res.sendFile(path.join(rootDir, 'views', 'users.html'));
+});
+
+router.post('/users', (req, res, next) => {
+ console.log(req.body);
+ res.redirect('/');
+});
+
+module.exports = router;
